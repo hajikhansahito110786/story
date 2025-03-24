63560bb7b90cb40d189ea8dc57a473511e67565e
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2025-03-24 12:10:14 +0500

    Add files via upload

---
+++ story/app.py
@@ -0,0 +1,11 @@
+name:str=str(input("name please"))
+city:str=str(input("city please"))
+verb:str=str(input("verb doing please"))
+animal:str=str(input("animal please"))
+adjective=str(input("adjective please"))
+
+print(f"One day {name} went to {city} zoo where he saw {adjective} {animal} was {verb} ")
+
+
+
+
